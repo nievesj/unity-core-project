0313e99d9649b079c6a72a13004d342128c9e575
José M. Nieves <jose.nieves@gmail.com>
2018-01-24 14:48:38 -0600

    Creating a custom event wrapping the Subject functionality.

---
+++ Tools/Events/Event.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+namespace Core.Events
+{
+	public class Event<T> : IDisposable
+	{
+		readonly Subject<T> _subject = new Subject<T>();
+
+		public IDisposable Subscribe(Action<T> action)
+		{
+			return _subject
+				.AsObservable()
+				.Subscribe(action);
+		}
+
+		public void Publish(T o)
+		{
+			_subject.OnNext(o);
+		}
+
+		public void OnComplete()
+		{
+			_subject.OnCompleted();
+		}
+
+		public void OnError(Exception ex)
+		{
+			_subject.OnError(ex);
+		}
+
+		public void Dispose()
+		{
+			_subject.Dispose();
+		}
+	}
+}
