581603fc3592981f371555abcf80fa330eeee3e3
Jose M. Nieves <jose.nieves@gmail.com>
2018-01-31 23:22:31 -0600

    Removing old references left over from before decoupling this from my game. Adding more comments.

--- Tools/Pooling/Pooler.cs
+++ Tools/Pooling/Pooler.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using MatchGame;
 using UnityEngine;
 
 namespace Core.Polling
@@ -10,21 +9,30 @@ namespace Core.Polling
 		protected Stack<T> pool;
 		protected GameObject prefab;
 
-		protected Transform pollerTransform;
-		public Transform PollerTransform { get { return pollerTransform; } }
+		protected Transform poolerTransform;
+		public Transform PoolerTransform { get { return poolerTransform; } }
 
 		private int sizeLimit = 1;
 
-		public Pooler(GameObject prefab, int amount)
+		/// <summary>
+		/// Initialize pooler
+		/// </summary>
+		/// <param name="pre">Gameobject to be pooled</param>
+		/// <param name="amount">Pool size</param>
+		public Pooler(GameObject pre, int amount)
 		{
-			var go = new GameObject(Constants.PooledObject + prefab.name);
-			pollerTransform = go.transform;
-			this.prefab = prefab;
+			var go = new GameObject(Constants.PooledObject + pre.name);
+			poolerTransform = go.transform;
+			prefab = pre;
 			sizeLimit = amount;
 
 			CreatePool(amount);
 		}
 
+		/// <summary>
+		/// Get element from the pool
+		/// </summary>
+		/// <returns></returns>
 		public T Pop()
 		{
 			if (pool.Count == 0)
@@ -33,6 +41,10 @@ namespace Core.Polling
 			return Get();
 		}
 
+		/// <summary>
+		/// Get element from the pool, if there are no more elements allocated, create a new one
+		/// </summary>
+		/// <returns></returns>
 		public T PopResize()
 		{
 			sizeLimit++;
@@ -42,50 +54,12 @@ namespace Core.Polling
 			return Get();
 		}
 
-		private T Get()
-		{
-			T obj = pool.Pop();
-			obj.gameObject.SetActive(true);
-
-			return obj;
-		}
-
-		public void Push(T obj)
-		{
-			obj.gameObject.SetActive(false);
-
-			if (!pool.Count.Equals(sizeLimit))
-				pool.Push(obj);
-			else
-				GameObject.Destroy(obj.gameObject);
-		}
-
-		public void Destroy()
-		{
-			DestroyPool();
-
-			if (pollerTransform)
-				GameObject.Destroy(pollerTransform.gameObject);
-		}
-
-		protected void CreatePool(int amount)
-		{
-			if (pool != null)
-				DestroyPool();
-
-			pool = new Stack<T>();
-
-			for (int i = 0; i <= amount - 1; i++)
-				pool.Push(CreateObject(prefab));
-		}
-
-		protected T CreateObject(GameObject prefab)
-		{
-			GameObject go = GameObject.Instantiate(prefab, pollerTransform);
-			go.SetActive(false);
-			return go.GetComponent<T>()as T;
-		}
-
+		/// <summary>
+		/// Resize pool.This changes the size of the pool, however, if there are elements alive that
+		/// have been pooled, they will stay alive until they are pushed back into the pool
+		/// at which moment they will be destroyed if they dont fit in the ne wpool size
+		/// </summary>
+		/// <param name="val">New pool size</param>
 		public void ResizePool(int val)
 		{
 			sizeLimit = val;
@@ -105,7 +79,58 @@ namespace Core.Polling
 			}
 		}
 
-		protected void DestroyPool()
+		/// <summary>
+		/// Return element to the fool
+		/// </summary>
+		/// <param name="obj"></param>
+		public void Push(T obj)
+		{
+			obj.gameObject.SetActive(false);
+
+			if (!pool.Count.Equals(sizeLimit))
+				pool.Push(obj);
+			else
+				GameObject.Destroy(obj.gameObject);
+		}
+
+		/// <summary>
+		/// Destroy pool
+		/// </summary>
+		public void Destroy()
+		{
+			DestroyPool();
+
+			if (poolerTransform)
+				GameObject.Destroy(poolerTransform.gameObject);
+		}
+
+		private T Get()
+		{
+			T obj = pool.Pop();
+			obj.gameObject.SetActive(true);
+
+			return obj;
+		}
+
+		private void CreatePool(int amount)
+		{
+			if (pool != null)
+				DestroyPool();
+
+			pool = new Stack<T>();
+
+			for (int i = 0; i <= amount - 1; i++)
+				pool.Push(CreateObject(prefab));
+		}
+
+		private T CreateObject(GameObject prefab)
+		{
+			GameObject go = GameObject.Instantiate(prefab, poolerTransform);
+			go.SetActive(false);
+			return go.GetComponent<T>()as T;
+		}
+
+		private void DestroyPool()
 		{
 			foreach (var obj in pool)
 			{
