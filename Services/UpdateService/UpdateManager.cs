40fc67aea6ab02cb8542e00e01e97ccd2c1e1855
José M. Nieves <jose.nieves@gmail.com>
2018-05-13 09:10:36 -0500

    improving update manager

--- Services/UpdateService/UpdateManager.cs
+++ Services/UpdateService/UpdateManager.cs
@@ -3,118 +3,109 @@ using UnityEngine;
 
 namespace Core.Services.UpdateManager
 {
-	public class UpdateManager : MonoBehaviour
-	{
-		private List<BehaviourDelegateType> update;
-		private List<BehaviourDelegateType> fixedUpdate;
-		private List<BehaviourDelegateType> lateUpdate;
+    public class UpdateManager : MonoBehaviour
+    {
+        private List<BehaviourDelegateType> update;
+        private List<BehaviourDelegateType> fixedUpdate;
+        private List<BehaviourDelegateType> lateUpdate;
 
-		private void Awake()
-		{
-			update = new List<BehaviourDelegateType>();
-			fixedUpdate = new List<BehaviourDelegateType>();
-			lateUpdate = new List<BehaviourDelegateType>();
-		}
+        private void Awake()
+        {
+            update = new List<BehaviourDelegateType>();
+            fixedUpdate = new List<BehaviourDelegateType>();
+            lateUpdate = new List<BehaviourDelegateType>();
+        }
 
-		private void Update()
-		{
-			if (update.Count > 0)
-			{
-				for (int i = 0; i < update.Count; i++)
-				{
-					if (!update[i].behaviour)
-						continue;
+        private void Update()
+        {
+            if (update == null || update.Count <= 0) return;
 
-					update[i].updateMethod();
-				}
-			}
-		}
+            for (var i = 0; i < update.Count; i++)
+            {
+                if (!update[i].behaviour)
+                    update[i].updateMethod();
+            }
+        }
 
-		private void FixedUpdate()
-		{
-			if (fixedUpdate.Count > 0)
-			{
-				for (int i = 0; i < fixedUpdate.Count; i++)
-				{
-					if (!fixedUpdate[i].behaviour)
-						continue;
+        private void FixedUpdate()
+        {
+            if (fixedUpdate == null || fixedUpdate.Count <= 0) return;
 
-					fixedUpdate[i].updateMethod();
-				}
-			}
-		}
+            for (var i = 0; i < fixedUpdate.Count; i++)
+            {
+                if (fixedUpdate[i].behaviour)
+                    fixedUpdate[i].updateMethod();
+            }
+        }
 
-		private void LateUpdate()
-		{
-			if (lateUpdate.Count > 0)
-			{
-				for (int i = 0; i < lateUpdate.Count; i++)
-				{
-					if (!lateUpdate[i].behaviour)
-						continue;
+        private void LateUpdate()
+        {
+            if (lateUpdate == null || lateUpdate.Count <= 0) return;
 
-					lateUpdate[i].updateMethod();
-				}
-			}
-		}
+            for (var i = 0; i < lateUpdate.Count; i++)
+            {
+                if (lateUpdate[i].behaviour)
+                    lateUpdate[i].updateMethod();
+            }
+        }
 
-		public void Attach(BehaviourDelegateType behaviourDelegateType)
-		{
-			if (!Contains(behaviourDelegateType))
-			{
-				switch (behaviourDelegateType.type)
-				{
-					case UpdateType.Update:
-						update.Add(behaviourDelegateType);
-						break;
+        public void Attach(BehaviourDelegateType behaviourDelegateType)
+        {
+            if (!Contains(behaviourDelegateType))
+            {
+                switch (behaviourDelegateType.type)
+                {
+                    case UpdateType.Update:
+                        update.Add(behaviourDelegateType);
+                        break;
 
-					case UpdateType.FixedUpdate:
-						fixedUpdate.Add(behaviourDelegateType);
-						break;
+                    case UpdateType.FixedUpdate:
+                        fixedUpdate.Add(behaviourDelegateType);
+                        break;
 
-					case UpdateType.LateUpdate:
-						lateUpdate.Add(behaviourDelegateType);
-						break;
-				}
-			}
-		}
+                    case UpdateType.LateUpdate:
+                        lateUpdate.Add(behaviourDelegateType);
+                        break;
+                }
+            }
+        }
 
-		public void Detach(BehaviourDelegateType behaviourDelegateType)
-		{
-			if (Contains(behaviourDelegateType))
-			{
-				switch (behaviourDelegateType.type)
-				{
-					case UpdateType.Update:
-						update.Remove(behaviourDelegateType);
-						break;
+        public void Detach(BehaviourDelegateType behaviourDelegateType)
+        {
+            if (Contains(behaviourDelegateType))
+            {
+                switch (behaviourDelegateType.type)
+                {
+                    case UpdateType.Update:
+                        update.Remove(behaviourDelegateType);
+                        break;
 
-					case UpdateType.FixedUpdate:
-						fixedUpdate.Remove(behaviourDelegateType);
-						break;
+                    case UpdateType.FixedUpdate:
+                        fixedUpdate.Remove(behaviourDelegateType);
+                        break;
 
-					case UpdateType.LateUpdate:
-						lateUpdate.Remove(behaviourDelegateType);
-						break;
-				}
-			}
-		}
+                    case UpdateType.LateUpdate:
+                        lateUpdate.Remove(behaviourDelegateType);
+                        break;
+                }
+            }
+        }
 
-		private bool Contains(BehaviourDelegateType behaviour)
-		{
-			switch (behaviour.type)
-			{
-				case UpdateType.Update:
-					return update.Contains(behaviour);
+        private bool Contains(BehaviourDelegateType behaviour)
+        {
+            switch (behaviour.type)
+            {
+                case UpdateType.Update:
+                    return update.Contains(behaviour);
 
-				case UpdateType.FixedUpdate:
-					return fixedUpdate.Contains(behaviour);
+                case UpdateType.FixedUpdate:
+                    return fixedUpdate.Contains(behaviour);
 
-				case UpdateType.LateUpdate:
-					return lateUpdate.Contains(behaviour);
-			}
+                case UpdateType.LateUpdate:
+                    return lateUpdate.Contains(behaviour);
+            }
 
-			return false;
-		}
-	}
+            return false;
+        }
+    }
 }
