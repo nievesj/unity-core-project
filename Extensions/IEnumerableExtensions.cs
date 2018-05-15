32ea8816b8830fc0858f13ac692a634aa43ceb3e
Jose M. Nieves <jose.nieves@gmail.com>
2018-05-15 10:57:54 -0500

    Added a IEnumerable extension to get a random elements from an IEnumerable, and a BetweenExtension, that returns true/false if a number is in between two elements.

---
+++ Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class IEnumerableExtensions
+{
+    /// <summary>
+    /// Gets a random element from an IEnumerable object
+    /// </summary>
+    /// <param name="source"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns>Random T</returns>
+    public static T GetRandomElement<T>(this IEnumerable<T> source)
+    {
+        var enumerable = source.ToList();
+        return enumerable.ElementAt(Random.Range(0, enumerable.Count()));
+    }
+}
